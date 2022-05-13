671bdcbff28f70a174b213ab16ad90189195e2a7
u1973239 <u1973239@campus.udg.edu>
2022-05-13 14:54:12 +0200

    Moviment

---
+++ js/scenes/game.js
@@ -0,0 +1,51 @@
+
+class GameScene extends Phaser.Scene {
+    constructor (){
+        super('GameScene');
+        this.speed = 100;
+        this.player = null;
+        this.cursors = null;
+    }
+
+    preload (){	
+		this.load.image('Personatge', '../resources/Personatge.png');
+		this.load.image('SpriteMovimiento', '../resources/SpriteMovimiento.svg');
+        this.load.image('Mapa', '../resources/MapaJPG.jpg');
+	}
+	
+    create (){
+
+        this.background = this.add.image(400,300,'Mapa');
+   
+	    this.player = this.physics.add.sprite(150 ,370,'Personatge');
+
+        this.player.setScale(0.5);
+		this.player.setCollideWorldBounds(true);
+
+        this.background.displayWidth = this.sys.canvas.width;
+        this.background.displayHeight = this.sys.canvas.height;
+        
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+	}
+	
+	update (){
+        if (this.cursors.left.isDown){
+            this.player.setVelocityX(-160);
+            //this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown){
+            this.player.setVelocityX(160);
+            //this.player.anims.play('right', true);
+        }
+        else{
+            this.player.setVelocityX(0);
+            //this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down)
+            this.player.setVelocityY(-330);
+    
+    }
+}
